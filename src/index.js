afd9d00c2fac44bf01c91be95c55fe7133a6fa45
junifar <junifar@gmail.com>
2018-04-19 21:00:42 +0700

    Adding react-router-dom package Adding 404 Error Not Found

--- src/index.js
+++ src/index.js
@@ -8,7 +8,17 @@ import Contoh from './contoh/Contoh';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min';
 import 'bootstrap/dist/js/bootstrap.min';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Content from "./contoh/Content";
+import NotFound from "./not_found/NotFound";
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Contoh />, document.getElementById('root'));
+ReactDOM.render((
+        <Router>
+            <switch>
+                <Route exact path="/" component={Contoh}/>
+                <Route exact path="/test" component={Content}/>
+                <Route component={NotFound} />
+            </switch>
+        </Router>
+    ), document.getElementById('root'));
 // registerServiceWorker();
---
+++ src/not_found/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+    render()
+    {
+        return(
+            <h1>Page Not Found Gan</h1>
+        );
+    }
+}
+
+export default NotFound;
